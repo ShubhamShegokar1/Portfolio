651f297fdae4b8fb6e36f5066db97bbd53bc22ac
ShubhamShegokar1 <shubhamsandy.1@gmail.com>
2021-12-14 16:58:58 +0530

    View Source code

--- Javascript.js
+++ Javascript.js
@@ -8,6 +8,15 @@ var TxtType = function(el, toRotate, period) {
     this.isDeleting = false;
 };
 
+
+var message = "function disabled";
+
+function rtclickcheck(keyp){ if (navigator.appName == "Netscape" && keyp.which == 3){ alert(message); return false; }
+
+if (navigator.appVersion.indexOf("MSIE") != -1 && event.button == 2) { alert(message); return false; } }
+
+document.onmousedown = rtclickcheck;
+
 TxtType.prototype.tick = function() {
     var i = this.loopNum % this.toRotate.length;
     var fullTxt = this.toRotate[i];
